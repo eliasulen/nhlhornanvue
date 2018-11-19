74eb4278925f1160f7986ab1ce9064cddab7d4cd
Elias Ulén <elias.ulen@gmail.com>
2018-11-19 23:26:28 +0100

    Fill datatable with ApiClient

--- src/components/Odds.vue
+++ src/components/Odds.vue
@@ -1,49 +1,53 @@
 
 <template>
-  <v-card>
-    <v-card-title>
-      Odds
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="search"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title>
-    <v-data-table
-    :headers="headers"
+   <v-card>
+      <v-card-title>
+        Odds
+        <v-spacer></v-spacer>
+        <v-text-field
+          append-icon="search"
+          label="Search"
+          single-line
+          hide-details
+          v-model="search"
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
         :items="items"
         :search="search"
-    >
-      <template slot="items" slot-scope="props">
-        <td>{{ props.item.name }}</td>
-        <td class="text-xs-right">{{ props.item.key }}</td>
-      </template>
-      <v-alert slot="no-results" :value="true" color="error" icon="warning">
-        Your search for "{{ search }}" found no results.
-      </v-alert>
-    </v-data-table>
-  </v-card>
+        loading
+      >
+       <v-progress-linear slot="progress" color="blue" indeterminate v-show="isLoading"></v-progress-linear>
+        <template slot="items" slot-scope="props">
+          <td class="text-xs">{{ props.item.key }}</td>
+          <td class="text-xs">{{ props.item.date }}</td>
+        </template>
+        <v-alert slot="no-results" :value="true" color="error" icon="warning">
+          Your search for "{{ search }}" found no results.
+        </v-alert>
+      </v-data-table>
+    </v-card>
 </template>
 
 <script>
 import ApiClient from '../api/index'
 
   export default {
-  created: function(){
+   created: function(){
+      this.isLoading = true;
       this.getData()
   },
   methods: {
       getData: function(){
-                        var vm = this;
-                        ApiClient().get('Odds/2018-2019')
-                        .then(function (data) {
-                            vm.items = data;
-                        }).catch(function (error) {
-    console.log(error);
-  });;                             
+          var vm = this;
+                        ApiClient().get('Odds')
+                        .then(function (response) {
+                            vm.items = response.data;
+                            console.log('Fetched')
+                        }).catch(function (error) {             
+         console.log(error);
+  });                             
 
       }
   },
@@ -51,16 +55,15 @@ import ApiClient from '../api/index'
     return {
       search: '',
       headers: [
-      /*  {
-          text: 'Dessert (100g serving)',
-          align: 'left',
-          sortable: false,
-          value: 'name'
-        },*/
-        { text: 'Key', value: 'key' }
+      { text: 'Key', value: 'key', sortable: false, searchable: false },
+      { text: 'Date', value: 'date'}
       ],
       items: []
     }
+  },
+  mounted()
+  {
+      this.isLoading = false;
   }
   }
 </script>
