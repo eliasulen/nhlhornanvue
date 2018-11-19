2fd4e3c50290f882eea4d1f5242138421a2dd740
Elias Ulén <elias.ulen@gmail.com>
2018-11-19 23:35:33 +0100

    Trigger heroku deploy

--- src/components/TeamOdds.vue
+++ src/components/TeamOdds.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="teamodds">
     <h1>This is an teamodds page</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+    <p>Lorem ipsum dolor sitt amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
   </div>
 </template>
